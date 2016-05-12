97df2bdf56e5b0cb93377cf795532b4a07912cd3
Abilis <abilis@mail.ru>
2016-05-13 00:31:00 +0400

    Светофор написан

--- src/ru/java/semaphore/Semaphore.java
+++ src/ru/java/semaphore/Semaphore.java
@@ -10,18 +10,21 @@ import java.io.InputStreamReader;
 public class Semaphore {
 
     private BufferedReader reader;
-    private int amountMinute;
+    private float amountMinute;
+    private float normalAmountMinute;
     private static final String GREATING_MESSAGE = "Первые 2 минуты каждого часа горит зеленый цвет сфетовора," +
-            " затем 3 - желтый, затем 5 - красный.\nВведите количество минут прошедших с начала часа, и программа" +
-            " покажет цвет сфетофора в эту минуту";
+            " затем 3 - желтый, затем 5 - красный.\nВведите количество минут, прошедших с начала часа, и программа" +
+            " покажет цвет сфетофора в этот момент.\nКоличество минут может быть дробным";
+    private String calculatedColor;
 
     public void init() {
         reader = new BufferedReader(new InputStreamReader(System.in));
         this.writeGreatingMessage();
         this.initAmountMinute();
+        this.calculateAmountMinuteInOneHour();
+        this.calculateAmountMinuteInTenMinute();
         this.calculateColor();
-
-
+        this.writeResult();
     }
 
     private void writeGreatingMessage() {
@@ -32,7 +35,7 @@ public class Semaphore {
 
         while (true) {
             try {
-                amountMinute = Integer.parseInt(reader.readLine());
+                amountMinute = Float.parseFloat(reader.readLine());
                 break;
             } catch (IOException | NumberFormatException e) {
                 ConsoleHelper.writeMessageWithBl("Некорректные данные. Попробуйте еще раз");
@@ -40,7 +43,39 @@ public class Semaphore {
         }
     }
 
-    private void calculateColor() {
+    /**
+     * метод вычисляет количество минут, прошедших с начала последнего часа, т.к. в течение каждого часа
+     * все события одинаковы
+     */
+    private void calculateAmountMinuteInOneHour() {
+        this.normalAmountMinute = amountMinute % 60;
+    }
+
+    /**
+     * метод вычисляет количество минут, прошедших с начала очередной десятиминутки, т.е. все события повторяются
+     * с периодичностью 10 минут
+     */
+    private void calculateAmountMinuteInTenMinute() {
+        this.normalAmountMinute %= 10;
+    }
+
+    private String calculateColor() {
+
+        if (normalAmountMinute < 2) {
+            calculatedColor = "зеленый";
+        }
+        else if (normalAmountMinute < 5) {
+            calculatedColor = "желтый";
+        }
+        else {
+            calculatedColor = "красный";
+        }
+
+        return null;
+    }
+
+    private void writeResult() {
+        ConsoleHelper.writeMessageWithBl("На минуте " + amountMinute + " горит " + calculatedColor + " цвет");
 
     }
 }
